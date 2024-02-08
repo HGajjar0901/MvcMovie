eb7a6fb042a1a173ba9c96c390ca407dd7d08d81
himan <himan@WINDOWSISG>
2024-02-08 15:25:11 -0500

    Add project files.

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "Leo",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "Romantic Comedy",
+                    Rating = "8",
+                    Price = 7.99M
+                },
+                new Movie
+                {
+                    Title = "Moana",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Rating = "9",
+
+                    Price = 8.99M
+                },
+                new Movie
+                {
+                    Title = "Zindgi na milegi dubara",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                    Rating = "6",
+                    Price = 9.99M
+                },
+                new Movie
+                {
+                    Title = "Argyle",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Rating = "5",
+                    Price = 3.99M
+                }
+             
+            ); 
+            context.SaveChanges();
+        }
+    }
+}
---
+++ MvcMovie/README.txt
@@ -0,0 +1,26 @@
+﻿
+------ This is the README file for MVCMovie Web Application ------
+
+------ 2024-01-17 1339 ------
+
+- Done Installation 
+
+------ 2024-01-25 1113 ------
+
+- Add a controller
+- Add a View 
+- Add a model 
+
+-This Displayed List of Movies 
+
+------ 2024-01-30 1320 ------
+
+- Added Git account 
+- added Initial migration
+- In Part 4, add a model to an ASP.NET Core MVC app I faced an error of "SQL Server" , So I started from scratch 
+
+------ 2024-02-08  1230 ------
+
+- Started whole application from scratche 
+- Facing an errror while adding the view in ""Views/HelloWorld/Welcome.cshtml" Section then again I started from scratch without Not get Panic :)
+
